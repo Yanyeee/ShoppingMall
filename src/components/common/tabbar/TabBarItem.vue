03d8a9ac18dd03515e3b6b7054867430f0c952a0
Yanyeee <773652561@qq.com>
2020-10-24 12:23:43 +0800

    初始化项目

---
+++ src/components/common/tabbar/TabBarItem.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="tab_bar_item" @click="itemClick">
+    <div class="item_icon" v-show="!isActive">
+      <slot name="item_icon"></slot>
+    </div>
+    <div class="item_icon_active" v-show="isActive">
+      <slot name="active_icon"></slot>
+    </div>
+    <div class="item_text" :style="activeStyle">
+      <slot name="item_text"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TabBarItem",
+  props: {
+    path: String,
+  },
+  computed: {
+    isActive() {
+      return this.$route.path.indexOf(this.path) !== -1;
+    },
+    activeStyle() {
+      return this.isActive ? { color: "#ff5777" } : {};
+    },
+  },
+  methods: {
+    itemClick() {
+      this.$router.replace(this.path);
+    },
+  },
+};
+</script>
+
+<style>
+.tab_bar_item {
+  flex: 1;
+  font-size: 14px;
+}
+.tab_bar_item img,
+.item_icon_active img {
+  width: 24px;
+  height: 24px;
+  margin-top: 5px;
+  vertical-align: middle;
+}
+.item-text {
+  font-size: 12px;
+  margin-top: 3px;
+  color: #333;
+}
+</style>
---
+++ src/components/content/mainTabbar/MainTabBar.vue
@@ -0,0 +1,64 @@
+<template>
+  <TabBar>
+    <TabBarItem path="/home">
+      <template v-slot:item_icon>
+        <img src="~assets/img/tabbar/home.svg" alt="" />
+      </template>
+      <template v-slot:active_icon>
+        <img src="~assets/img/tabbar/home_active.svg" alt="" />
+      </template>
+      <template v-slot:item_text>
+        <div>首页</div>
+      </template>
+    </TabBarItem>
+    <TabBarItem path="/category">
+      <template v-slot:item_icon>
+        <img src="~assets/img/tabbar/category.svg" alt="" />
+      </template>
+      <template v-slot:active_icon>
+        <img src="~assets/img/tabbar/category_active.svg" alt="" />
+      </template>
+      <template v-slot:item_text>
+        <div>分类</div>
+      </template>
+    </TabBarItem>
+    <TabBarItem path="/cart">
+      <template v-slot:item_icon>
+        <img src="~assets/img/tabbar/shopcart.svg" alt="" />
+      </template>
+      <template v-slot:active_icon>
+        <img src="~assets/img/tabbar/shopcart_active.svg" alt="" />
+      </template>
+      <template v-slot:item_text>
+        <div>购物车</div>
+      </template>
+    </TabBarItem>
+    <TabBarItem path="/profile">
+      <template v-slot:item_icon>
+        <img src="~assets/img/tabbar/profile.svg" alt="" />
+      </template>
+      <template v-slot:active_icon>
+        <img src="~assets/img/tabbar/profile_active.svg" alt="" />
+      </template>
+      <template v-slot:item_text>
+        <div>我的</div>
+      </template>
+    </TabBarItem>
+  </TabBar>
+</template>
+
+<script>
+import TabBar from "components/common/tabbar/TabBar";
+import TabBarItem from "components/common/tabbar/TabBarItem";
+
+export default {
+  name: "MainTabBar",
+  components: {
+    TabBar,
+    TabBarItem,
+  },
+};
+</script>
+
+<style>
+</style>
